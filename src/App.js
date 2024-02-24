402e9b4172ecdb7486bf6f9f0bd714ad0701dc65
My Name <ashutoshdewangan25@gmail.com>
2024-02-24 22:41:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import {useRef, useState} from "react";
 
 function App() {
+
+  let min=0;
+  let sec=0;
+  let hr=0;
+  let timerid=useRef(null);
+
+  let display=`0${hr}:0${min}:0${sec}`
+  const [val,setVal]=useState(display)
+
+  const handleBtn=(event)=>{
+    if(event.target.name==='start'){
+        timerid.current=setInterval(()=>{
+        sec++;
+        if(sec>58){
+          sec=0;
+          min++;
+        }
+        if(min>58){
+          min=0;
+          hr++;
+        }
+        setVal(`${hr<10 ? `0${hr}`:hr}:${min<10 ? `0${min}`:min}:${sec<10 ? `0${sec}`:sec}`)
+      },1000)
+    }
+
+    if(event.target.name==='stop'){
+      clearInterval(timerid.current)
+      
+    }
+    if(event.target.name==='end'){
+       clearInterval(timerid.current)
+       setVal(`00:00:00`)
+    }
+
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Stop Watch Timer</h1>
+      <p>{val}</p>
+      <div className='btn'>
+        <button name='start' className='start-btn' onClick={handleBtn}>Start</button>
+        <button name='stop' className='stop-btn' onClick={handleBtn}>Stop</button>
+        <button name='end' className='end-btn' onClick={handleBtn}>End</button>
+      </div>
     </div>
   );
 }
